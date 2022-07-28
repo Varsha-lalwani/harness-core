a3c4b4c66100f88f3943615d199bfcd5ebcc0285
Arvind Choudhary <arvind.choudhary@harness.io>
2022-07-28 14:56:13 +0530

    [CDS-34033]: Fixed code style check; Added docs

--- 126-instance/src/main/java/io/harness/dtos/deploymentinfo/DeploymentInfoDTO.java
+++ 126-instance/src/main/java/io/harness/dtos/deploymentinfo/DeploymentInfoDTO.java
@@ -9,6 +9,7 @@ package io.harness.dtos.deploymentinfo;
 
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
