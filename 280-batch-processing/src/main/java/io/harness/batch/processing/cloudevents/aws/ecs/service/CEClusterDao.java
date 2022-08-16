5cf30f52dbab671a8c670f46446d221bdb275d21
Hitesh Aringa <hitesh.aringa@harness.io>
2022-08-16 14:15:53 -0700

    [CCM-8614]: Soft delete ECS clusters

--- 280-batch-processing/src/main/java/io/harness/batch/processing/cloudevents/aws/ecs/service/CEClusterDao.java
+++ 280-batch-processing/src/main/java/io/harness/batch/processing/cloudevents/aws/ecs/service/CEClusterDao.java
@@ -19,6 +19,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateOperations;
 
 @Slf4j
 public class CEClusterDao {
