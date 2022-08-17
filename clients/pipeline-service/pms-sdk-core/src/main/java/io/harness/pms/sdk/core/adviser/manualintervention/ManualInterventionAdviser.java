5addc268a2d94fe9b358d8be2bff9c5dc6512929
Sahil Hindwani <sahil.hindwani@harness.io>
2022-08-17 18:33:05 +0530

    [PIE-4382]: Addition of new failure strategy for execution time inputs and some refactoring

--- clients/pipeline-service/pms-sdk-core/src/main/java/io/harness/pms/sdk/core/adviser/manualintervention/ManualInterventionAdviser.java
+++ clients/pipeline-service/pms-sdk-core/src/main/java/io/harness/pms/sdk/core/adviser/manualintervention/ManualInterventionAdviser.java
@@ -59,7 +59,7 @@ public class ManualInterventionAdviser implements Adviser {
   @Override
   public boolean canAdvise(AdvisingEvent advisingEvent) {
     boolean canAdvise = (StatusUtils.brokeStatuses().contains(advisingEvent.getToStatus())
-                            || StatusUtils.isAdvisingStatus(advisingEvent.getFromStatus())
+                            || StatusUtils.isAdvisingStatus(advisingEvent.getFromStatus()))
         && advisingEvent.getFromStatus() != INTERVENTION_WAITING;
     ManualInterventionAdviserParameters parameters = extractParameters(advisingEvent);
     List<FailureType> failureTypesList = getAllFailureTypes(advisingEvent);
