adf2f7bc6d06918e778ad91457a5eeb513e42a00
Rishabh Gupta <rishabh.gupta@harness.io>
2022-08-17 15:09:50 +0530

    [CDS-35145]: fixed infra variables

--- 125-cd-nextgen/src/main/java/io/harness/cdng/infra/yaml/DeploymentTemplateInfrastructure.java
+++ 125-cd-nextgen/src/main/java/io/harness/cdng/infra/yaml/DeploymentTemplateInfrastructure.java
@@ -1,9 +1,17 @@
+/*
+ * Copyright 2022 Harness Inc. All rights reserved.
+ * Use of this source code is governed by the PolyForm Free Trial 1.0.0 license
+ * that can be found in the licenses directory at the root of this repository, also available at
+ * https://polyformproject.org/wp-content/uploads/2020/05/PolyForm-Free-Trial-1.0.0.txt.
+ */
+
 package io.harness.cdng.infra.yaml;
+
+import static io.harness.data.structure.EmptyPredicate.isEmpty;
 
 import io.harness.annotation.RecasterAlias;
 import io.harness.annotations.dev.HarnessTeam;
 import io.harness.annotations.dev.OwnedBy;
-import io.harness.beans.SwaggerConstants;
 import io.harness.cdng.infra.beans.DeploymentTemplateInfraMapping;
 import io.harness.cdng.infra.beans.InfraMapping;
 import io.harness.cdng.infra.beans.InfrastructureDetailsAbstract;
@@ -13,10 +21,12 @@ import io.harness.ng.core.infrastructure.InfrastructureKind;
 import io.harness.pms.yaml.ParameterField;
 import io.harness.walktree.visitor.SimpleVisitorHelper;
 import io.harness.walktree.visitor.Visitable;
+import io.harness.yaml.core.variables.NGVariable;
 
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import io.swagger.annotations.ApiModelProperty;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import javax.validation.constraints.NotNull;
 import lombok.Builder;
@@ -34,15 +44,15 @@ import org.springframework.data.annotation.TypeAlias;
 @RecasterAlias("io.harness.cdng.infra.yaml.DeploymentTemplateInfrastructure")
 public class DeploymentTemplateInfrastructure
     extends InfrastructureDetailsAbstract implements Infrastructure, Visitable, WithConnectorRef {
-  @NotNull
-  @NotEmpty
-  @ApiModelProperty(dataType = SwaggerConstants.STRING_MAP_CLASSPATH)
-  @Wither
-  ParameterField<Map<String, String>> variables;
+  @NotNull @NotEmpty @Wither List<NGVariable> variables;
 
   @Override
   public InfraMapping getInfraMapping() {
-    return DeploymentTemplateInfraMapping.builder().variables(variables.getValue()).build();
+    Map<String, String> infraVars = new HashMap<>();
+    for (NGVariable variable : variables) {
+      infraVars.put(variable.getName(), variable.getCurrentValue().toString());
+    }
+    return DeploymentTemplateInfraMapping.builder().variables(infraVars).build();
   }
 
   @Override
@@ -53,7 +63,11 @@ public class DeploymentTemplateInfrastructure
 
   @Override
   public String[] getInfrastructureKeyValues() {
-    return variables.getValue().keySet().toArray(new String[0]);
+    List<String> infraKeys = new ArrayList<>();
+    for (NGVariable variable : variables) {
+      infraKeys.add(variable.getName());
+    }
+    return infraKeys.toArray(new String[0]);
   }
 
   @Override
@@ -65,7 +79,7 @@ public class DeploymentTemplateInfrastructure
   public Infrastructure applyOverrides(Infrastructure overrideConfig) {
     DeploymentTemplateInfrastructure config = (DeploymentTemplateInfrastructure) overrideConfig;
     DeploymentTemplateInfrastructure resultantInfra = this;
-    if (!ParameterField.isNull(config.getVariables())) {
+    if (!isEmpty(config.getVariables())) {
       resultantInfra = resultantInfra.withVariables(config.getVariables());
     }
     return resultantInfra;
